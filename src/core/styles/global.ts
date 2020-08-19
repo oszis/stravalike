934f876b7284ab2b9b726c021c5782a58b3752a0
Глеб Белкин <belkin643@gmail.com>
2020-08-20 00:51:05 +0700

    removed unnecessarry

--- src/core/styles/global.ts
+++ src/core/styles/global.ts
@@ -3,4 +3,19 @@ import {normalize} from "./normalize";
 
 export const GlobalStyles = createGlobalStyle`
   ${normalize}
+  
+  body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+
 `;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as RouterProvider} from 'react-router-dom';
-import './index.css';
 import App from './pages/App.routing';
-import * as serviceWorker from './serviceWorker';
 import { GlobalStyles } from './core/styles/global';
 
 ReactDOM.render(
@@ -15,8 +13,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
